6fe524ec127495b81dfa8369fd85ba136a73ef36
Alexander Thompson <legoboyjr@gmail.com>
2018-10-24 18:35:47 -0400

    finish player dial

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //veriables
 const board = document.querySelector('.board');
 const cells = board.querySelectorAll('.cell');
-const turnDisplay = document.querySelector('.turn');
+const turnDisplay = document.querySelector('.turn span');
 const controls = document.querySelector('.control');
 let currentPlayer = 'X';
 let gameOver = false;
@@ -13,7 +13,18 @@ cells.forEach(function addEventListener(Cell){
 //functions
 function onCellClick(event) {
     const element = event.target;
+    if(element.beenClicked){
+        return;
+    }
+    element.beenClicked = true;
     element.textContent = currentPlayer;
     currentPlayer = currentPlayer === 'X' ? 'O': 'X';
+    renderTurn();
+}
+
+function renderTurn(){
+    turnDisplay.textContent = currentPlayer;
 }
 //kick it off
+
+renderTurn();
