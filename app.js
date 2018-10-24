00efe7176d2b8e4fb130bf7de9d24a69214390a4
Alexander Thompson <legoboyjr@gmail.com>
2018-10-23 20:55:32 -0400

    started js XO

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log ('its working');
+//veriables
+const board = document.querySelector('.board');
+const cells = board.querySelectorAll('.cell');
+const turnDisplay = document.querySelector('.turn');
+const controls = document.querySelector('.control');
+let currentPlayer = 'X';
+let gameOver = false;
+
+//events 
+cells.forEach(function addEventListener(Cell){
+    Cell.addEventListener('click', onCellClick);
+});
+//functions
+function onCellClick(event) {
+    const element = event.target;
+    element.textContent = currentPlayer;
+    currentPlayer = currentPlayer === 'X' ? 'O': 'X';
+}
+//kick it off
