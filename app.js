437c8b88f0db90d4e9d742ad2ac1d4d07cffee64
Alexander Thompson <legoboyjr@gmail.com>
2018-10-25 19:01:23 -0400

    extract info from dom in js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ let grid = makeGrid();
 function Cell(template){
     const data = {
         template: template,
+        textContent: '',
         hasBeenClicked: false
     };
     return data;
@@ -22,13 +23,20 @@ resetBtn.addEventListener('click', reset);
 //functions
 function onCellClick(event) {
     const element = event.target;
-    if(element.beenClicked){
+    const dataset = element.dataset;
+    const index = dataset.index;
+    const cell = grid[index];
+    if( !cell || (index !== 0 && !index)  ) {
         return;
     }
-    element.beenClicked = true;
-    element.textContent = currentPlayer;
+    if(cell.hasBeenClicked){
+        return;
+    }
+    cell.hasBeenClicked = true;
+    cell.textContent = currentPlayer;
     currentPlayer = currentPlayer === 'X' ? 'O': 'X';
     renderTurn();
+    render();
 }
 
 function reset(event) {
@@ -40,15 +48,15 @@ function reset(event) {
 
 function makeGrid(){
     return [
-        Cell(`<div class="cell h30 w30 bl-none bt-none"></div>`), 
-        Cell(`<div class="cell h30 w30 bt-none"></div>`), 
-        Cell(`<div class="cell h30 w30 bt-none br-none"></div>`),
-        Cell(`<div class="cell h30 w30 bl-none"></div>`), 
-        Cell(`<div class="cell h30 w30"></div>`), 
-        Cell(`<div class="cell h30 w30 br-none"></div>`),
-        Cell(`<div class="cell h30 w30 bl-none bb-none"></div>`), 
-        Cell(`<div class="cell h30 w30 bb-none"></div>`),
-        Cell(`<div class="cell h30 w30 bb-none br-none"></div>`)
+        Cell(`<div class="cell h30 w30 bl-none bt-none" data-index="0"></div>`), 
+        Cell(`<div class="cell h30 w30 bt-none" data-index="1"></div>`), 
+        Cell(`<div class="cell h30 w30 bt-none br-none" data-index="2"></div>`),
+        Cell(`<div class="cell h30 w30 bl-none" data-index="3"></div>`), 
+        Cell(`<div class="cell h30 w30" data-index="4"></div>`), 
+        Cell(`<div class="cell h30 w30 br-none" data-index="5"></div>`),
+        Cell(`<div class="cell h30 w30 bl-none bb-none" data-index="6"></div>`), 
+        Cell(`<div class="cell h30 w30 bb-none" data-index="7"></div>`),
+        Cell(`<div class="cell h30 w30 bb-none br-none" data-index="8"></div>`)
     ];
 }
 
@@ -58,7 +66,9 @@ function renderTurn(){
 
 function render() {
     board.innerHTML = grid.map(function(element) {
-        return element.template;
+        const [first,last] = element.template.split('><');
+        const html = `${first}>${element.textContent}<${last}`;
+        return html;
     }).join('\n');
 }
 
