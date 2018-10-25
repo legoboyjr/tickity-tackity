32ca305dc4c61546c5f05a7b2d7d0513a104b526
Alexander Thompson <legoboyjr@gmail.com>
2018-10-24 20:19:28 -0400

    re did cells

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ const controls = document.querySelector('.controls');
 const resetBtn = controls.querySelector('button');
 let currentPlayer = 'X';
 let gameOver = false;
+let grid = [
+    Cell(`<div class="cell h30 w30 bl-none bt-none"></div>`), 
+    Cell(`<div class="cell h30 w30 bt-none"></div>`), 
+    Cell(`<div class="cell h30 w30 bt-none br-none"></div>`),
+    Cell(`<div class="cell h30 w30 bl-none"></div>`), 
+    Cell(`<div class="cell h30 w30"></div>`), 
+    Cell(`<div class="cell h30 w30 br-none"></div>`),
+    Cell(`<div class="cell h30 w30 bl-none bb-none"></div>`), 
+    Cell(`<div class="cell h30 w30 bb-none"></div>`),
+    Cell(`<div class="cell h30 w30 bb-none br-none"></div>`)
+];
+
+function Cell(template){
+    const data = {
+        template: template,
+        hasBeenClicked: false
+    };
+    return data;
+}
+
 
 //events 
 cells.forEach(function addEventListener(Cell){
@@ -36,6 +56,13 @@ function reset(event) {
 function renderTurn(){
     turnDisplay.textContent = currentPlayer;
 }
+
+function render() {
+    board.innerHTML = grid.map(function(element) {
+        return element.template;
+    }).join('\n');
+}
+
 //kick it off
 
 renderTurn();
